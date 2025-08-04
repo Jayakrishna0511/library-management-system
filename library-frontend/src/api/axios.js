6c87939429814fdeb6b34e20b9182108ebd5aa7c
jayakrishna0511 <dontharaboinajayakrishna@gmail.com>
2025-08-04 12:39:32 +0530

    Fixed end point

--- library-frontend/src/api/axios.js
+++ library-frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://library-management-system-pi4l.onrender.com',
+  baseURL: 'https://library-management-system-pi4l.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
