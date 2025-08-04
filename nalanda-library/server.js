11bdf96fb3a85ef77e89e7f5d2f205518d2da23a
jayakrishna0511 <dontharaboinajayakrishna@gmail.com>
2025-08-04 12:28:03 +0530

    Fix: Add Vercel domain to CORS allowed origins

--- nalanda-library/server.js
+++ nalanda-library/server.js
@@ -10,9 +10,14 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://library-management-system-two-nu.vercel.app'  
+  ],
   credentials: true,
 }));
+
 
 app.use(express.json());
 
